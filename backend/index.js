194f321ce3dd71dcc356e905493e874f0d4f9438
inga-divra <ingady@gmail.com>
2025-04-15 12:57:22 +0300

    Restructure project: move frontend and backend to separate folders

--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const morgan = require('morgan');
+const path = require('path');
 const app = express();
 app.use(express.json());
 
 morgan.token('body', (req, res) => {
   return JSON.stringify(req.body);
 });
-app.use(express.static('dist'));
+//app.use(express.static('dist'));
+app.use(express.static(path.join(__dirname, 'dist')));
 app.use(morgan('tiny'));
 app.use(
   morgan(':method :url :status :res[content-length] - :response-time ms :body')
