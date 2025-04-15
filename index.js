a85f0d4bf5aaf489403cb2b9712f8dcce62ee6be
inga-divra <ingady@gmail.com>
2025-04-15 12:00:28 +0300

    feat: serve frontend by adding dist folder and express static middleware

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ app.use(express.json());
 morgan.token('body', (req, res) => {
   return JSON.stringify(req.body);
 });
-
+app.use(express.static('dist'));
 app.use(cors());
 app.use(morgan('tiny'));
 app.use(
