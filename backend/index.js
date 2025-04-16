5bc9d8b6ff7b8d8e2ce6e3ba22d78a03ac5c979f
inga-divra <ingady@gmail.com>
2025-04-16 13:06:08 +0300

    refactor: integrate MongoDB connection and Mongoose model for Person

--- backend/index.js
+++ backend/index.js
@@ -1,41 +1,32 @@
+require('dotenv').config();
 const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
 const morgan = require('morgan');
 const path = require('path');
-const app = express();
+const Person = require('./models/person');
+
+// Middleware
 app.use(express.json());
 
 morgan.token('body', (req, res) => {
   return JSON.stringify(req.body);
 });
-//app.use(express.static('dist'));
+
+// Serve static files from the 'dist' directory
 app.use(express.static(path.join(__dirname, 'dist')));
+
 app.use(morgan('tiny'));
 app.use(
   morgan(':method :url :status :res[content-length] - :response-time ms :body')
 );
 
-let persons = [
-  {
-    id: '1',
-    name: 'Arto Hellas',
-    number: '040-123456',
-  },
-  {
-    id: '2',
-    name: 'Ada Lovelace',
-    number: '39-44-5323523',
-  },
-  {
-    id: '3',
-    name: 'Dan Abramov',
-    number: '12-43-234345',
-  },
-  {
-    id: '4',
-    name: 'Mary Poppendieck',
-    number: '39-23-6423122',
-  },
-];
+// Get ALL PERSONS
+app.get('/api/persons', (req, res) => {
+  Person.find({}).then((persons) => {
+    res.json(persons);
+  });
+});
 
 app.get('/api/persons', (req, res) => {
   res.json(persons);
