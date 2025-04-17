6a473249cd553526f05371481feb0a363b90c12b
inga-divra <ingady@gmail.com>
2025-04-17 10:55:33 +0300

    feat(server): add validation error handling to error middleware

--- backend/index.js
+++ backend/index.js
@@ -111,9 +111,13 @@ const errorHandler = (error, request, response, next) => {
 
   if (error.name === 'CastError') {
     return response.status(400).send({ error: 'malformatted id' });
+  } else if (error.name === 'ValidationError') {
+    return response.status(400).json({ error: error.message });
   }
+
   next(error);
 };
+
 app.use(errorHandler);
 
 // Middleware for handling unknown endpoints
