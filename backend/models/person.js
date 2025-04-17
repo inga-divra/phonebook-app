f68f1440d2562ff6ba6f79b81fd8f672a973a785
inga-divra <ingady@gmail.com>
2025-04-17 16:57:02 +0300

    feat(validation): add custom phone number validation to Person schema

--- backend/models/person.js
+++ backend/models/person.js
@@ -20,7 +20,16 @@ const personSchema = new mongoose.Schema({
     minlength: 3,
     required: true,
   },
-  number: String,
+  number: {
+    type: String,
+    validate: {
+      validator: function (v) {
+        return /^\d{2,3}-\d+$/.test(v) && v.length >= 8;
+      },
+      message: (props) => `${props.value} is not a valid phone number!`,
+    },
+    required: [true, 'User phone number required'],
+  },
 });
 
 personSchema.set('toJSON', {
